fb3dc7e37d6e148b1e73c4eb23edbfd10e395cca
Karthik <dayanakarthik28@gmail.com>
2024-11-26 18:57:25 +0530

    Second commit

--- network/models.py
+++ network/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
-# Create your models here.
+REACTION_CHOICES = [
+    ('WOW', 'Wow'),
+    ('LIT', 'Lit'),
+    ('LOVE', 'Love'),
+    ('HAHA', 'Haha'),
+    ('THUMBS-UP', 'Thumbs Up'),
+    ('THUMBS-DOWN', 'Thumbs Down'),
+    ('ANGRY', 'Angry'),
+    ('SAD', 'Sad')
+]
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    profile_pic = models.CharField(max_length=100)
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(User)
+class Posts(models.Model):
+    content = models.CharField(max_length=1000)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_post = models.ForeignKey(Group,null=True,blank=True, on_delete=models.CASCADE)
+class Comments(models.Model):
+    content = models.CharField(max_length=1000)
+    commented_at = models.DateTimeField(auto_now_add=True)
+    commented_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_comment = models.ForeignKey(Posts, null=True, blank=True, on_delete=models.CASCADE)
+    self_comment = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+class Reactions(models.Model):
+    post_react = models.ForeignKey(Posts, null=True, blank=True, on_delete=models.CASCADE)
+    comment_react = models.ForeignKey(Comments, null=True, blank=True, on_delete=models.CASCADE)
+    reaction = models.CharField(max_length=100, choices=REACTION_CHOICES)
+    reacted_at = models.DateTimeField(auto_now_add=True)
+    reacted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+class Membership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+    class Meta:
+        unique_together = ('group', 'user')
--- network/views.py
+++ network/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from fb_post.models import User,Posts,Comments,Reactions
+from django.db.models import Count
+def create_post():
+    user=User.objects.all().first()
+    p=Posts.objects.create(posted_by=user,content='beautiful')
+    c=p.pk
+    return c
+def create_comment():
+    user=User.objects.all().first()
+    post=Posts.objects.all().first()
+    c=Comments.objects.create(content='good',commented_by=user,post_comment=post)
+    return c.pk
+def react_to_post():
+    user=User.objects.all().first()
+    post=Posts.objects.all().first()
+    r=Reactions.objects.create(post_react=post,reacted_by=user,reaction='WOW')
+    return r.pk
+def get_total_reaction_count():
+    t=Reactions.objects.aggregate(count=Count('reaction'))
+    return t
+def get_reaction_metrics():
+    p=Posts.objects.all().first()
+    r=Reactions.objects.filter(post_react=p).values('reaction').annotate(count=Count('reaction'))
+    return {reaction['reaction']:reaction['count'] for reaction in r}
+def get_posts_reacted_by_user(user_id):
+    p=Reactions.objects.filter(reacted_by=user_id).values_list('post_react_id',flat=True)
+    return p
+def get_reactions_to_post(post_id):
+    return Reactions.objects.filter(post_react=post_id).values('reacted_by_id','reacted_by__name','reacted_by__profile_pic','reaction')
+def get_posts_with_more_comments_than_reactions():
+    P=Posts.objects.annotate(count1=Count('comments'),count2=Count('reactions'))
+    return [p.id for p in P if p.count1>p.count2]
 
-# Create your views here.
+def run():
+    print(get_posts_with_more_comments_than_reactions())
